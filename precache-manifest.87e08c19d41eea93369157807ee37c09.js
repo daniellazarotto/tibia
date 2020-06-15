10e89c0c86fbcd242bf894490c8ceaafbec9e411
T35000119 <daniel.lazarotto@compasso.com.br>
2020-06-15 11:43:47 -0300

    Updates

---
+++ precache-manifest.87e08c19d41eea93369157807ee37c09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1993a16718ee24a102cc991680f54261",
+    "url": "/tibia/index.html"
+  },
+  {
+    "revision": "ccfac894791044373c44",
+    "url": "/tibia/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0152afc63d0e367c5ceb",
+    "url": "/tibia/static/js/2.78aeda83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tibia/static/js/2.78aeda83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccfac894791044373c44",
+    "url": "/tibia/static/js/main.ee842dae.chunk.js"
+  },
+  {
+    "revision": "f59a4005621193693152",
+    "url": "/tibia/static/js/runtime-main.ca632db8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tibia/static/media/logo.5d5d9eef.svg"
+  }
+]);
